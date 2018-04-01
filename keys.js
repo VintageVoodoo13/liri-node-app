c72533b219f6fead5336af5b0e9cef5344d77564
VintageVoodoo13 <32887419+VintageVoodoo13@users.noreply.github.com>
2018-03-31 20:04:42 -0500

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,7 @@
+exports.twitterKeys = {
+Consumer Key (API Key)	'TUqtaehfQ0SpHwrpgLtFw3CPM'
+App-only authentication	'https://api.twitter.com/oauth2/token'
+Request token URL	'https://api.twitter.com/oauth/request_token'
+Authorize URL	'https://api.twitter.com/oauth/authorize'
+Access token URL	'https://api.twitter.com/oauth/access_token'
+}
