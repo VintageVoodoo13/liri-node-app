e156bb0d0294fe9532d4e1cb1ccc2def74a884d1
VintageVoodoo13 <32887419+VintageVoodoo13@users.noreply.github.com>
2018-04-05 22:24:46 -0500

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log("Keys loaded");
+
+var twitterKeys = {
+    consumer_key:'uVDLT2cVMoOw2jzBMAxTFJwgN',
+    consumer_secret: 'FJdhNiGmc0gcU9fIZJ7FgJBGkqdcSzLAO6wgAiUqPhx1flP6Cj',
+    access_token_key: '980236450116579330-RA6DenkRkvuBfqJEu3AFVOejW2VGkYg',
+    access_token_secret: 'vueRAcKwqkoKE3Oinr33N6zodBbJN4g99WowGDU3TtXKP',
+
+//App-only authentication:	'https://api.twitter.com/oauth2/token',
+//Request token URL:	'https://api.twitter.com/oauth/request_token',
+//Authorize URL	'https://api.twitter.com/oauth/authorize',
+//Access token URL	'https://api.twitter.com/oauth/access_token',
+};
+
+module.exports = twitterKeys;
